dd7b2f9434b1f1a7fd03298366bd5f0091eb7cd5
diansonsatsoumouyane-cpu <diansonsatsoumouyane@gmail.com>
2025-12-03 20:46:58 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 3;
+    char op = '+';
+    int resultat;
+
+    switch(op) {
+        case '+':
+            resultat = num1 + num2;
+            break;
+        case '-':
+            resultat = num1 - num2;
+            break;
+        case '*':
+            resultat = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                resultat = num1 / num2;
+            else {
+                printf("Erreur : division par zero\n");
+                return 1;
+            }
+            break;
+        case '%':
+            if (num2 != 0)
+                resultat = num1 % num2;
+            else {
+                printf("Erreur : modulo par zero\n");
+                return 1;
+            }
+            break;
+        case '&':
+            resultat = num1 & num2;
+            break;
+        case '|':
+            resultat = num1 | num2;
+            break;
+        case '~':
+            resultat = ~num1; // opération NOT sur num1
+            break;
+        default:
+            printf("Operateur inconnu\n");
+            return 1;
+    }
+
+    printf("Resultat : %d\n", resultat);
+    return 0;
+}
+
 
